dcfad023b1b322b45f2dc689667e4707aa9ff1fc
sidney2k1 <sidsingla02@gmail.com>
2024-08-21 17:26:37 +0530

    sid

---
+++ int.py
@@ -0,0 +1,8 @@
+a=int(input("enter the number:"))
+if a<15:
+    print("a is smaller than 15")
+    print("i'm in an if block")
+else:
+    print("a is bigger than 15")
+    print("i'm in an else block")
+print("i'm neither in an if block nor an else block")
